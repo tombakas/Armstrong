f02dde0f1780ef0c525c21e21fbc17e9670e936f
Tomas Tombakas <tombakas@gmail.com>
2017-12-13 19:51:36 +0100

    Remove unecessary print

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 @app.route('/get-tables', methods=['POST'])
 def poop():
     data = request.get_json()
-    print(data)
     j = script.process_request(data)
     html = render_template("tables.html", table=j)
     return json.dumps({'success': True, 'data': html}), 200, {'ContentType': 'application/json'}

5d653197ddcd29687133d734d24b4353192c174b
Tomas Tombakas <tombakas@gmail.com>
2017-12-16 16:22:02 +0100

    Add error printing to flask app

--- app.py
+++ app.py
@@ -16,8 +16,14 @@ app = Flask(__name__)
 def poop():
     data = request.get_json()
     j = script.process_request(data)
-    html = render_template("tables.html", table=j)
-    return json.dumps({'success': True, 'data': html}), 200, {'ContentType': 'application/json'}
+    if "errors" in j:
+        e = []
+        for key, value in j["errors"].items():
+            e.append("{}: {}".format(key, value))
+        return json.dumps({'success': True, 'data': {"errors": e}}), 200, {'ContentType': 'application/json'}
+    else:
+        html = render_template("tables.html", table=j)
+        return json.dumps({'success': True, 'data': html}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/')
